fee194dd4e0599fc8f8c751a2cd22e0c48ce4142
Maria Priton <120567147+MariaPriton@users.noreply.github.com>
2024-03-16 13:44:36 +0530

    Add files via upload
    my firstCommit

---
+++ src/main/java/com/example/practiceservice1/controller/MailTemplateController.java
@@ -0,0 +1,31 @@
+package com.example.practiceservice1.controller;
+
+import com.example.practiceservice1.payload.DroolsLoanDetailsPayload;
+import com.example.practiceservice1.service.MailTemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping(path = "/api/mail")
+
+public class MailTemplateController {
+
+    @Autowired
+    public MailTemplateService mailTemplateService;
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHEllo() throws IOException {
+        return ResponseEntity.ok().body("Hello");
+    }
+    @PostMapping("/sentTemplatePdf")
+    public ResponseEntity<Boolean> sentTemplatePdf() throws IOException {
+        Boolean result = mailTemplateService.sentTemplatePdf();
+        return ResponseEntity.ok().body(result);
+    }
+}
